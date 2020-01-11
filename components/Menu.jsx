ba08aeab1b58665cd24b3bccf8049da590d3a85c
hoangndm <hoangndm99@gmail.com>
2020-01-11 09:35:53 +0700

    update

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -7,9 +7,43 @@ import base from '../static/assets/styles/base';
 
 export default class Menu extends Component<Props> {
     render() {
-        const { routeName } = this.props.navigation.state;
         return (
             <View style={[ base.w100, styles.container, base.row, base.py5, base.zIndex10 ]}>
+                <View style={{ flex: 1, padding: 5, width: '25%' }}>
+                    <TouchableOpacity>
+                        <View style={ styles.img }>
+                            <Image style={{ resizeMode: 'contain', width: 20, height: 20 }}
+                                source={ require('../static/images/icon-home.png') }/>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={{ flex: 1, padding: 5, width: '25%' }}>
+                    <TouchableOpacity>
+                        <View style={ styles.img }>
+                            <Image style={{ resizeMode: 'contain',width: 20, height: 20  }}
+                                source={ require('../static/images/icon-search-inac.png') }/>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={{ flex: 1, padding: 5, width: '25%' }}>
+                    <TouchableOpacity>
+                        <View style={ styles.img }>
+                            <Image style={{ resizeMode: 'contain', width: 20, height: 20  }}
+                                source={ require('../static/images/icon-noti-inac.png') }/>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={{ flex: 1, padding: 5, width: '25%' }}>
+                    <TouchableOpacity>
+                        <View style={ styles.img }>
+                            <Image style={{ resizeMode: 'contain', width: 20, height: 20 }}
+                                source={ require('../static/images/icon-user-inac.png') }/>
+                        </View>
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
